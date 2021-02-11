28faf523f498fe93573e827c7ca62d018af7eb41
jdog382 <jdog382@tutanota.com>
2021-02-11 09:15:25 +0200

    new colors

--- config.def.h
+++ config.def.h
@@ -11,11 +11,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#1d2021";
+static const char col_gray2[]       = "#32302f";
+static const char col_gray3[]       = "#a89984";
+static const char col_gray4[]       = "#f9f5d7";
+static const char col_cyan[]        = "#458588";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
